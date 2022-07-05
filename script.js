9de47f725e2caf9854695c70a947f792be298588
Nyi Zin Thant <codebynyi@gmail.com>
2022-07-05 15:46:54 +0630

    selecting element

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const domScore0 = document.getElementById('score--0');
+const domScore1 = document.getElementById('score--1');
+const domDice = document.querySelector('.dice');
+
+domDice.classList.add('hidden');
