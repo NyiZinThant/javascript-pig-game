f3d97496ac28da33dd35c8ccd9a3c909c137b139
Nyi Zin Thant <codebynyi@gmail.com>
2022-07-05 19:44:51 +0630

    refactor main function

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 'use strict';
 
-// state
-let playing = true;
-
-// select dice
-const domDice = document.querySelector('.dice');
-
-// select action btn
-const domBtnNew = document.querySelector('.btn--new');
-const domBtnRoll = document.querySelector('.btn--roll');
-const domBtnHold = document.querySelector('.btn--hold');
-
-// current status
-let current = 0;
-let currentPlayer = 0;
-const scores = [0, 0];
-
 // custom functions
 const switchPlayer = function () {
   current = 0;
@@ -48,10 +32,36 @@ const endGame = function () {
   domDice.classList.add('hidden');
 };
 
-// starting condition
-getPlayer(0, 'score').textContent = 0;
-getPlayer(1, 'score').textContent = 0;
-domDice.classList.add('hidden');
+// select dice
+const domDice = document.querySelector('.dice');
+
+// select action btn
+const domBtnNew = document.querySelector('.btn--new');
+const domBtnRoll = document.querySelector('.btn--roll');
+const domBtnHold = document.querySelector('.btn--hold');
+
+// current status
+let current, currentPlayer, scores, playing;
+
+// main function
+const init = function () {
+  scores = [0, 0];
+  playing = true;
+  current = 0;
+  currentPlayer = 0;
+  domDice.classList.add('hidden');
+  for (let i = 0; i < scores.length; i++) {
+    getPlayer(i, 'score').textContent = 0;
+    getPlayer(i, 'current').textContent = 0;
+    getPlayer(i, 'player').classList.remove('player--winner');
+    getPlayer(i, 'player').classList.remove('player--active');
+  }
+  getPlayer(0, 'player').classList.add('player--active');
+};
+init();
+
+// restart game
+domBtnNew.addEventListener('click', init);
 
 // Rolling function
 domBtnRoll.addEventListener('click', function () {
@@ -82,26 +92,4 @@ domBtnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
-});
-
-domBtnNew.addEventListener('click', function () {
-  playing = true;
-  if (getPlayer(currentPlayer, 'player').classList.contains('player--active')) {
-    getPlayer(currentPlayer, 'player').classList.remove('player--active');
-  }
-  getPlayer(0, 'player').classList.add('player--active');
-  for (let i = 0; i < scores.length; i++) {
-    scores[i] = 0;
-    getPlayer(i, 'score').textContent = 0;
-
-    current = 0;
-    getPlayer(i, 'current').textContent = 0;
-
-    if (
-      getPlayer(currentPlayer, 'player').classList.contains('player--winner')
-    ) {
-      getPlayer(currentPlayer, 'player').classList.remove('player--winner');
-    }
-  }
-  currentPlayer = 0;
 });
